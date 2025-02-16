8400cf2b34509fe327db3d18fbb3452ad875e43d
Aramís Concepción Durán <aramis@systemli.org>
2025-02-16 07:40:29 +0100

    Unterbinde Double-tap zoom

--- deploy.fish
+++ deploy.fish
@@ -1,4 +1,4 @@
 #!/usr/bin/env fish
 
-gren make src/Main.gren --optimize --output=index.html
-scp index.html root@hub.nojs.de:/var/www/dedris.nojs.de/
+gren0.4.4 make src/Main.gren --optimize --output=app.js
+#scp app.js index.html root@hub.nojs.de:/var/www/dedris.nojs.de/
