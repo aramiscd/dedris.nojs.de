8f29b31910ef479083d44248285c9131d01bde3b
Aramís Concepción Durán <aramis@systemli.org>
2025-08-01 07:00:45 +0200

    Zwischenstand

---
+++ build.fish
@@ -0,0 +1,2 @@
+#!/usr/bin/env fish
+gren0.4.4 make src/Main.gren --output=app.js $argv
--- deploy.fish
+++ deploy.fish
@@ -1,4 +1,4 @@
 #!/usr/bin/env fish
 
-gren0.4.4 make src/Main.gren --optimize --output=app.js
-#scp app.js index.html root@hub.nojs.de:/var/www/dedris.nojs.de/
+./build.fish --optimize
+and scp app.js index.html root@hub.nojs.de:/var/www/dedris.nojs.de/
